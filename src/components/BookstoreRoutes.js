410b7e6864d41de38b040aebc36e9c2e136bfd8b
Suchan Park <coolsu92@gmail.com>
2023-10-18 15:46:19 +1300

    add: Shopping cart component and styling

--- src/components/BookstoreRoutes.js
+++ src/components/BookstoreRoutes.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import CartPage from "../pages/CartPage";
+
 
 function BookstoreRoutes() {
     const location = useLocation();
     return (
         <Routes location={location} key={location.pathname}>
             <Route path="/" element={<HomePage />} />
+            <Route path="/cart" element={<CartPage />} />
         </Routes>
     );
 }
